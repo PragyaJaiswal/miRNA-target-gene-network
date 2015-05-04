235701da5bed5fad47bee90ccb7fb2f9e3f943a4
PragyaJaiswal <pragya.jswl@gmail.com>
2015-05-04 14:58:13 +0530

    Minor changes in jsonify.

--- network.py
+++ network.py
@@ -22,27 +22,33 @@ def remove_duplicates(viruses):
 
 '''
 map_dict : Contains miRNA names as keys and their target genes as values.
+Maps miRNA to their target genes.
 '''
 
-def mapper():
+def mirna_mapper():
 	for i, j in enumerate(mirna):
 		map_dict.setdefault(j, []).append(genes[i])
 		# if i == 25:
 		# 	break
-	jsonify(map_dict)
+	jsonify(map_dict, 'mirna_map_dict.json')
 	return map_dict
 
+def gene_mapper():
+	for i, j in enumerate(genes):
+		pass
 
 # Export dictionary to JSON for showing count in a presentable form.
 def jsonify(dict, location=None):
 	a = json.dumps(dict, sort_keys=True, indent=4, separators=(',', ': '))
+	filename = str(location.split('.')[0])
 	if location == None:
 		with open('map_dict.json', 'w') as outfile:
 			outfile.write(a)
-		with open('map_dict.py', 'w') as outfile:
-			outfile.write(a)
 	else:
 		with open(str(location), 'a+') as outfile:
+			outfile.write(a)
+		with open(str(filename) + '.py', 'a+') as outfile:
+			outfile.write(filename + ' = ')
 			outfile.write(a)
 	# print(a)
 
@@ -52,4 +58,4 @@ if __name__ == '__main__':
 	mirna = open('./dat/hsa/miRNA.txt', 'r').read().splitlines()
 	# print('Genes original: ' + str(len(genes)))
 	# print('miRNA original: ' + str(len(mirna)))
-	map_dict = mapper()
+	mirna_map_dict = mirna_mapper()
