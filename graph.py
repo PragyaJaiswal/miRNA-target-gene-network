8251c3a04b8d84af07c92f3b231b877dcfa14cfc
PragyaJaiswal <pragya.jswl@gmail.com>
2015-05-05 06:02:40 +0530

    Started with visualization.

--- graph.py
+++ graph.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-from map_dict import map_dict
+from mirna_map_dict import mirna_map_dict
 
-G = nx.star_graph(10)
-pos = nx.spring_layout(G)
-colors = range(10)
-nx.draw(G, pos, node_color='#A0CBE2', edge_color=colors, width=4, edge_cmap=plt.cm.Reds, with_labels=False)
-# plt.savefig('edge_colormap.png')
-plt.show()
+for x in mirna_map_dict.keys():
+	targets = len(mirna_map_dict[x])
+	G = nx.star_graph(targets)
+	pos = nx.spring_layout(G)
+	colors = range(targets)
+	nx.draw(G, pos, node_color='#A0CBE2', edge_color=colors, width=4, edge_cmap=plt.cm.Reds, with_labels=False)
+	# plt.savefig('edge_colormap.png')
+	plt.show()
+	break
