364f47e024c1b7a62838637b07ed6e34cc227d48
Pragya Jaiswal <pragya.jswl@gmail.com>
2015-11-25 13:26:25 +0530

    The possibl expression levels that could be mapped from the data in the .\mRNA_concentration_data folder

---
+++ mirna_host_gene_map_with_expression_levels.py
@@ -0,0 +1,622 @@
+mirna_host_gene_map_with_expression_levels = {
+    "hsa-let-7f-2-3p": {
+        "Gene": "HUWE1",
+        "Gene Expression Level": 1160.62
+    },
+    "hsa-let-7g-3p": {
+        "Gene": "WDR82",
+        "Gene Expression Level": ""
+    },
+    "hsa-let-7g-5p": {
+        "Gene": "WDR82",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-106b-3p": {
+        "Gene": "MCM7",
+        "Gene Expression Level": 22138.87
+    },
+    "hsa-miR-106b-5p": {
+        "Gene": "MCM7",
+        "Gene Expression Level": 22138.87
+    },
+    "hsa-miR-10a-3p": {
+        "Gene": "HOXB3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-10a-5p": {
+        "Gene": "HOXB3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-10b-3p": {
+        "Gene": "HOXD4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-10b-5p": {
+        "Gene": "HOXD4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1226-3p": {
+        "Gene": "DHX30",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1226-5p": {
+        "Gene": "DHX30",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1227-3p": {
+        "Gene": "PLEKHJ1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1228-3p": {
+        "Gene": "LRP1",
+        "Gene Expression Level": 3293.46
+    },
+    "hsa-miR-1229-3p": {
+        "Gene": "MGAT4B",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1236-3p": {
+        "Gene": "RDBP",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1237-3p": {
+        "Gene": "RPS6KA4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1249": {
+        "Gene": "C22orf9",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1250-5p": {
+        "Gene": "AATK",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1256": {
+        "Gene": "EIF4G3",
+        "Gene Expression Level": 2205.3
+    },
+    "hsa-miR-126-3p": {
+        "Gene": "EGFL7",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-126-5p": {
+        "Gene": "EGFL7",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1262": {
+        "Gene": "GPR177",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1276": {
+        "Gene": "KLHL25",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1292-5p": {
+        "Gene": "NOP56",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1293": {
+        "Gene": "LIMA1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1296-5p": {
+        "Gene": "JMJD1C",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-1301-3p": {
+        "Gene": "DNMT3A",
+        "Gene Expression Level": 1311.76
+    },
+    "hsa-miR-1304-5p": {
+        "Gene": "TAF1D",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-139-3p": {
+        "Gene": "PDE2A",
+        "Gene Expression Level": 71.3
+    },
+    "hsa-miR-139-5p": {
+        "Gene": "PDE2A",
+        "Gene Expression Level": 71.3
+    },
+    "hsa-miR-140-3p": {
+        "Gene": "WWP2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-140-5p": {
+        "Gene": "WWP2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-141-3p": {
+        "Gene": "PTPN6",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-148b-3p": {
+        "Gene": "COPZ1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-148b-5p": {
+        "Gene": "COPZ1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-149-3p": {
+        "Gene": "GPC1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-149-5p": {
+        "Gene": "GPC1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-152-3p": {
+        "Gene": "COPZ2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-15a-3p": {
+        "Gene": "DLEU2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-15a-5p": {
+        "Gene": "DLEU2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-15b-3p": {
+        "Gene": "SMC4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-15b-5p": {
+        "Gene": "SMC4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-16-1-3p": {
+        "Gene": "DLEU2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-16-2-3p": {
+        "Gene": "SMC4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-188-3p": {
+        "Gene": "CLCN5",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-188-5p": {
+        "Gene": "CLCN5",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-191-3p": {
+        "Gene": "DALRD3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-191-5p": {
+        "Gene": "DALRD3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-192-5p": {
+        "Gene": "ATG2A",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-196b-3p": {
+        "Gene": "HOXA10",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-196b-5p": {
+        "Gene": "HOXA10",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-197-3p": {
+        "Gene": "GNAI3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-198": {
+        "Gene": "FSTL1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-200c-3p": {
+        "Gene": "PTPN6",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-204-5p": {
+        "Gene": "TRPM3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-208a-3p": {
+        "Gene": "MYH6",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-208b-3p": {
+        "Gene": "MYH7",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-21-3p": {
+        "Gene": "TMEM49",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-21-5p": {
+        "Gene": "TMEM49",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-211-5p": {
+        "Gene": "TRPM1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-218-1-3p": {
+        "Gene": "SLIT2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-22-3p": {
+        "Gene": "C17orf91",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-22-5p": {
+        "Gene": "C17orf91",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-224-5p": {
+        "Gene": "GABRE",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-23b-3p": {
+        "Gene": "C9orf3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-23b-5p": {
+        "Gene": "C9orf3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-24-1-5p": {
+        "Gene": "C9orf3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-25-3p": {
+        "Gene": "MCM7",
+        "Gene Expression Level": 22138.87
+    },
+    "hsa-miR-25-5p": {
+        "Gene": "MCM7",
+        "Gene Expression Level": 22138.87
+    },
+    "hsa-miR-26a-2-3p": {
+        "Gene": "CTDSP2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-26b-3p": {
+        "Gene": "CTDSP1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-26b-5p": {
+        "Gene": "CTDSP1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-27b-3p": {
+        "Gene": "C9orf3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-27b-5p": {
+        "Gene": "C9orf3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-28-3p": {
+        "Gene": "LPP",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-28-5p": {
+        "Gene": "LPP",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-30c-1-3p": {
+        "Gene": "NFYC",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-30e-3p": {
+        "Gene": "NFYC",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-30e-5p": {
+        "Gene": "NFYC",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-32-5p": {
+        "Gene": "C9orf5",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-326": {
+        "Gene": "ARRB1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-330-3p": {
+        "Gene": "EML2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-330-5p": {
+        "Gene": "EML2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-335-5p": {
+        "Gene": "MEST",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-338-3p": {
+        "Gene": "AATK",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-338-5p": {
+        "Gene": "AATK",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-33a-3p": {
+        "Gene": "SREBF2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-33a-5p": {
+        "Gene": "SREBF2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-33b-5p": {
+        "Gene": "SREBF1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-340-3p": {
+        "Gene": "RNF130",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-340-5p": {
+        "Gene": "RNF130",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-342-3p": {
+        "Gene": "EVL",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-342-5p": {
+        "Gene": "EVL",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-425-3p": {
+        "Gene": "DALRD3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-448": {
+        "Gene": "HTR2C",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-454-3p": {
+        "Gene": "FAM33A",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-454-5p": {
+        "Gene": "FAM33A",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-484": {
+        "Gene": "NDE1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-488-3p": {
+        "Gene": "ASTN1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-488-5p": {
+        "Gene": "ASTN1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-489-3p": {
+        "Gene": "CALCR",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-504-5p": {
+        "Gene": "FGF13",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-548j-5p": {
+        "Gene": "TPST2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-559": {
+        "Gene": "TACSTD1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-569": {
+        "Gene": "TNIK",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-577": {
+        "Gene": "UGT8",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-584-5p": {
+        "Gene": "SH3TC2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-589-3p": {
+        "Gene": "FBXL18",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-589-5p": {
+        "Gene": "FBXL18",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-595": {
+        "Gene": "PTPRN2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-597-5p": {
+        "Gene": "TNKS",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-603": {
+        "Gene": "KIAA1217",
+        "Gene Expression Level": 280.51
+    },
+    "hsa-miR-605-5p": {
+        "Gene": "PRKG1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-608": {
+        "Gene": "SEMA4G",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-613": {
+        "Gene": "APOLD1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-616-3p": {
+        "Gene": "DDIT3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-616-5p": {
+        "Gene": "DDIT3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-617": {
+        "Gene": "LIN7A",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-618": {
+        "Gene": "LIN7A",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-620": {
+        "Gene": "MED13L",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-624-3p": {
+        "Gene": "STRN3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-624-5p": {
+        "Gene": "STRN3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-625-3p": {
+        "Gene": "FUT8",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-625-5p": {
+        "Gene": "FUT8",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-626": {
+        "Gene": "MGA",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-629-3p": {
+        "Gene": "TLE3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-629-5p": {
+        "Gene": "TLE3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-630": {
+        "Gene": "ARIH1",
+        "Gene Expression Level": 2595.15
+    },
+    "hsa-miR-631": {
+        "Gene": "MAN2C1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-636": {
+        "Gene": "SFRS2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-638": {
+        "Gene": "DNM2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-641": {
+        "Gene": "AKT2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-657": {
+        "Gene": "AATK",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-660-5p": {
+        "Gene": "CLCN5",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-661": {
+        "Gene": "PLEC1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-662": {
+        "Gene": "MSLN",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-7-1-3p": {
+        "Gene": "HNRNPK",
+        "Gene Expression Level": 6323.02
+    },
+    "hsa-miR-708-3p": {
+        "Gene": "ODZ4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-708-5p": {
+        "Gene": "ODZ4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-744-3p": {
+        "Gene": "MAP2K4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-744-5p": {
+        "Gene": "MAP2K4",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-765": {
+        "Gene": "ARHGEF11",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-766-3p": {
+        "Gene": "SEPT6",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-874-3p": {
+        "Gene": "KLHL3",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-877-3p": {
+        "Gene": "ABCF1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-877-5p": {
+        "Gene": "ABCF1",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-887-3p": {
+        "Gene": "FBXL7",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-93-3p": {
+        "Gene": "MCM7",
+        "Gene Expression Level": 22138.87
+    },
+    "hsa-miR-93-5p": {
+        "Gene": "MCM7",
+        "Gene Expression Level": 22138.87
+    },
+    "hsa-miR-937-3p": {
+        "Gene": "SCRIB",
+        "Gene Expression Level": 2199.78
+    },
+    "hsa-miR-939-5p": {
+        "Gene": "CPSF1",
+        "Gene Expression Level": 4330.8
+    },
+    "hsa-miR-942-5p": {
+        "Gene": "TTF2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-943": {
+        "Gene": "WHSC2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-95-3p": {
+        "Gene": "ABLIM2",
+        "Gene Expression Level": ""
+    },
+    "hsa-miR-98-5p": {
+        "Gene": "HUWE1",
+        "Gene Expression Level": 1160.62
+    }
+}
