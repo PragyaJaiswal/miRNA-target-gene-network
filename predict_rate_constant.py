9b72f51a6d0f3d17a4f9a52989eba905d97b6e1a
Pragya Jaiswal <pragya.jswl@gmail.com>
2015-11-26 10:55:54 +0530

    Predict keq and the mmi expression level based on transcript count.

--- predict_rate_constant.py
+++ predict_rate_constant.py
@@ -15,20 +15,31 @@ def find_k_equivalent():
 			print targets
 			# print predicted_map[mirna][targets][19]
 			del_g_binding = predicted_map[mirna][targets][19]
-			keq = math.exp(-1 * del_g_binding/(0.008314 * 298))
+			keq = float(math.exp(-1 * del_g_binding/(0.008314 * 298)))
 			# print del_g_binding
 			# print keq
 			predicted_map[mirna][targets].append(keq)
+			predicted_map[mirna][targets].append(0)	# Putting mmi = 0 by default for each mirna-target gene combination so that each list has same structure.
 			# print predicted_map[mirna][targets]
-			'''
-			if not mirna_host_gene_map_with_transcript_count[mirna]["Host Gene"] == "":
-				# ------- m = NOT KNOWN ----------
-				mi = mirna_host_gene_map_with_transcript_count[mirna]["Gene Transcript Count"]
-				mmi = keq * m * mi
-				print mmi
-			'''
-	jsonify(predicted_map, 'predicted_with_keq.py', 'predicted_map_with_keq')
+			if mirna in mirna_host_gene_map_with_transcript_count.keys():
+				if not mirna_host_gene_map_with_transcript_count[mirna]["Host Gene"] == "":
+					m = find_target_gene_expression(targets)
+					mi = float(mirna_host_gene_map_with_transcript_count[mirna]["Gene Transcript Count"])
+					mmi = keq * m * mi
+					predicted_map[mirna][targets].append(mmi)
+	jsonify(predicted_map, 'predicted_with_keq_mmi_exp.py', 'predicted_map_with_keq_mmi_exp')
 			# raw_input('Enter')
+
+
+def find_target_gene_expression(target_gene):
+	with open('./data_used_for_mapping/gene_coordinates_from_ensembl.tsv', 'r') as gene_file:
+		gene_data = csv.reader(gene_file, dialect = 'excel-tab', skipinitialspace = True)
+		next(gene_data, None)
+		for each_line in gene_data:
+			if each_line[4] == target_gene:
+				return float(each_line[5])
+		gene_file.seek(0)
+				# return each_line[5]
 
 
 def jsonify(dictionary, filename, text='None'):
@@ -42,7 +53,4 @@ def jsonify(dictionary, filename, text='None'):
 
 
 if __name__ == '__main__':
-	# with open('predicted.py', 'r') as infile:
-	# 	data = json.loads(infile.read())
-	# print data
 	find_k_equivalent()
