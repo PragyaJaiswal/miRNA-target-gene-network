c95e043a370b67faf739b881730c0038279c32cb
Pragya Jaiswal <pragya.jswl@gmail.com>
2016-01-14 02:27:54 +0530

    Added code for including Transcript count in gene meta data

--- generate_meta_data.py
+++ generate_meta_data.py
@@ -11,6 +11,7 @@ from mirna_host_target_gene_expression import mirna_host_target_gene_expression
 
 accession_map = {}
 family_accession_map = {}
+all_target_genes = {}
 
 def append_data():
 	for mirna in mirna_host_target_gene_expression:
@@ -31,6 +32,7 @@ def append_data():
 			else:
 				mirna_host_target_gene_expression[mirna]["mature miRNA entry"] = None
 	jsonify(mirna_host_target_gene_expression, 'miRNA_meta_data.py', 'miRNA_meta_data')
+	jsonify(mirna_host_target_gene_expression, 'miRNA_meta_data.json')
 
 
 def jsonify(dictionary, filename, text='None'):
@@ -42,9 +44,32 @@ def jsonify(dictionary, filename, text='None'):
 			outfile.write(text + ' = ')
 			outfile.write(a)
 
+
+# def include_gene_transcript_count_to_gene_metadata():
+# 	for mirna in mirna_host_target_gene_expression.keys():
+# 		if "Host Gene":
+# 			pass
+# 		if not mirna_host_target_gene_expression[mirna]["Host Gene"] == '' :
+# 			all_target_genes[mirna_host_target_gene_expression[mirna]["Host Gene"]] = mirna_host_target_gene_expression[mirna]["Host Gene Transcript Count"]
+# 		if "Target Gene with Transcript Count" in mirna_host_target_gene_expression[mirna].keys():
+# 			for each in mirna_host_target_gene_expression[mirna]["Target Gene with Transcript Count"]:
+# 				all_target_genes[each[0]] = each[1]
+# 		# print all_target_genes
+# 	with open('gene_ID_Store.json') as gene_data:
+# 		data = json.load(gene_data)
+# 		for gene in all_target_genes:
+# 			if gene in data.keys():
+# 				data[gene]["Gene Transcript Count"] = all_target_genes[gene]
+# 			else:
+# 				data[gene]["Gene Transcript Count"] = None
+# 		jsonify(data, 'gene_ID_Store_transcript_count.py', 'gene_meta_data')
+
+
+
 if __name__ == '__main__':
 	for data in SeqIO.parse('C:\Users\Pragya\Documents\GitHub\miRNA-target-gene-network\mature\mature.fa', 'fasta'):
 		if 'Homo sapiens' in data.description:
+			
 			accession_map[(data.description).split()[0]] = (data.description).split()[1]
 	
 	with open('C:\Users\Pragya\Documents\GitHub\miRNA-target-gene-network\miFam\miFam.dat', 'r') as infile:
@@ -55,3 +80,4 @@ if __name__ == '__main__':
 				if 'hsa' in lis[2]:
 					family_accession_map[lis[2]] = lis[1]
 	append_data()
+	# include_gene_transcript_count_to_gene_metadata()
